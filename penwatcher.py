2e031b528abb1ec2851f58d297c9fa2f1c8f6791
Satanowski <satanowski@gmail.com>
2011-03-09 11:55:35 +0100

    Working version

--- penwatcher.py
+++ penwatcher.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python
 
 STORE_DIR="/proc/scsi/usb-storage"
-AUTHORIZED= [
-    '08135200B2971D04',
-]
 
-import os,re,time
+AUTHORIZED= {
+    # 'SerialNumber':'DeviceName'
+    '08135200B2971D04':'RedPen',
+}
+
+import os,re,time,subprocess
+
+def dSerByName(name):
+    for k in AUTHORIZED.keys():
+        if AUTHORIZED[k]==name:
+            return k
+    return ""
+
 
 def get_auth_devices():
     #returns list of serial numbers of mounted devices that are authorized
-    devices = []
+    devices = {}
     for f in os.listdir(STORE_DIR):
         did = re.findall("Serial Number: ([A-Za-z0-9]+)", file("%s/%s" % (STORE_DIR,f), "r").read() )
-        if len(did)>0 and did[0] in AUTHORIZED: 
-            devices.append(did[0])
+        if len(did)>0 and did[0] in AUTHORIZED.keys():
+            devices[did[0]] = AUTHORIZED[did[0]]
     return devices
 
 
-
-def watch( period=1, onMount=None, onDemount=None):
+def watch( period=1, onMountHash=None, onDemountHash=None ):
     last_seen = get_auth_devices()
     print "currently mounted authorized devices: %s\n\nstarting to watch..." % (last_seen)
 
@@ -28,18 +36,47 @@ def watch( period=1, onMount=None, onDemount=None):
     def def_onDemount(dev):
         print "Device %s has been unmounted" % dev
     
-    if onMount == None: onMount = def_onMount
-    if onDemount == None: onDemount = def_onDemount
+    if onMountHash == None: 
+        default_method_hash = {}
+        for k in AUTHORIZED.keys():
+            default_method_hash[k] = def_onMount
+        onMountHash = default_method_hash
+
+    if onDemountHash == None: 
+        default_method_hash = {}
+        for k in AUTHORIZED.keys():
+            default_method_hash[k] = def_onDemount
+        onDemountHash = default_method_hash
 
     while True:
-        for d in get_auth_devices():
-            if not d in last_seen: #new device plugged in
-                onMount(d)
-        for d in last_seen:
-            if not d in get_auth_devices(): #device plugged off
-                onDemount(d)
-        last_seen = get_auth_devices()
-        time.sleep(period)
+        try:
+            for d in get_auth_devices().keys():
+                if not d in last_seen.keys(): #new device plugged in
+                    onMountHash[d](d)
+            for d in last_seen.keys():
+                if not d in get_auth_devices().keys(): #device plugged off
+                    onDemountHash[d](d)
+            last_seen = get_auth_devices()
+            time.sleep(period)
+        except KeyboardInterrupt:
+            print "Exiting"
+            break
+
+
+def onRedPenMount(dev):  
+    print "Device %s has been mounted" % (dev)
+    print "UnLocking screen..."
+    subprocess.call( ['gnome-screensaver-command', '--deactivate'] )
+
+
+def onRedPenDeMount(dev):
+    print "Device %s has been demounted" % (dev)
+    print "Locking screen..."
+    subprocess.call( ['gnome-screensaver-command', '--lock'] )
+
 
 if __name__ == "__main__":
-    watch()
+    watch(1,
+        {dSerByName('RedPen'):onRedPenMount},
+        {dSerByName('RedPen'):onRedPenDeMount}
+    )
